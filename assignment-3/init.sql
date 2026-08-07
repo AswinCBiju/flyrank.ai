ee54692b4a8595099540e7799a07c530cd7fe5b1
AswinCBiju <aswinchkarayil136@gmail.com>
2026-08-07 19:30:01 +0530

    created init.sql

---
+++ assignment-3/init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    done BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+INSERT INTO tasks (title, done) 
+SELECT 'Learn Docker & Postgres', false
+WHERE NOT EXISTS (SELECT 1 FROM tasks);
